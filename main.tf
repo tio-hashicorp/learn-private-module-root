45d016d47580ac673371c3f0494d9fc70769b452
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2022-09-16 13:50:50 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/innovation-lab/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
+  version = "1.0.1"
 }
